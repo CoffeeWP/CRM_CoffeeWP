2db8df155692a25d688e3b11a4055bbcb31e0953
Or Duan <orduani@gmail.com>
2014-03-25 00:46:58 +0200

    a

---
+++ DjangoCoffee/wsgi.py
@@ -0,0 +1,15 @@
+python manage.py runserver"""
+WSGI config for DjangoCoffee project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoCoffee.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
