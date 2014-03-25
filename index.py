4069813e7d1877fa22ee2f3b95ccb5c5b4c5c418
Or Duan <orduani@gmail.com>
2014-03-25 19:13:01 +0200

    CRM beta 1

--- index.py
+++
@@ -1,9 +0,0 @@
-#from django.core import management
-#management.call_command("startproject", "DjangoCoffee")
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return "Hello World from Python"
-
-
