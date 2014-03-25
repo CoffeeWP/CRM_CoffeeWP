3a3e5f328c79d9cc3aa43677f9b70e88f1721209
Or Duan <orduani@gmail.com>
2014-03-25 19:13:35 +0200

    CRM beta 1

---
+++ CRM/customers/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from customers.models import Customer, Project, Income, Outcome
+
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 1
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Customer Information', {'fields': ('name',
+                                             'phone_number',
+                                             'website',
+                                             'email',
+                                             'tasks',
+                                             'contact_date')}),
+    ]
+    inlines = [ProjectInline]
+admin.site.register(Customer, CustomerAdmin)
+
+
+class IncomeInline(admin.TabularInline):
+    model = Income
+    extra = 2
+
+
+class OutcomeInline(admin.TabularInline):
+    model = Outcome
+    extra = 2
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Project Information', {'fields': ('customer_name',
+                                            'name')}),
+    ]
+    inlines = [IncomeInline, OutcomeInline]
+    list_display = ('customer_name', 'name')
+admin.site.register(Project, ProjectAdmin)
+
+
+class IncomeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Income Information', {'fields': ('project_name',
+                                           'date',
+                                           'type',
+                                           'value',
+                                           'work_time',
+                                           'recipe',
+                                           'note')}),
+    ]
+admin.site.register(Income, IncomeAdmin)
+
+
+class OutcomeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Outcome Information', {'fields': ('project_name',
+                                            'date',
+                                            'type',
+                                            'value',
+                                            'note')}),
+    ]
+admin.site.register(Outcome, OutcomeAdmin)
---
+++ CRM/customers/models.py
@@ -0,0 +1,46 @@
+from pyexpat import model
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField('Name', max_length=70)
+    phone_number = models.CharField('Phone Number', max_length=20)
+    website = models.URLField('Website Address')
+    email = models.EmailField('E-Mail')
+    tasks = models.URLField('Tasks')
+    contact_date = models.DateTimeField('Contact Data')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Project(models.Model):
+    customer_name = models.ForeignKey(Customer)
+    name = models.CharField('Name', max_length=70)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Income(models.Model):
+    project_name = models.ForeignKey(Project)
+    date = models.DateTimeField('Date')
+    type = models.CharField('Type', max_length=100)
+    value = models.IntegerField('Value')
+    work_time = models.IntegerField('Work Time')
+    recipe = models.BooleanField('Recipe')
+    note = models.TextField('Note')
+
+    def __unicode__(self):
+        return self.type
+
+
+class Outcome(models.Model):
+    project_name = models.ForeignKey(Project)
+    date = models.DateTimeField('Date')
+    type = models.CharField('Type', max_length=100)
+    value = models.IntegerField('Value')
+    note = models.TextField('Note')
+
+    def __unicode__(self):
+        return self.type
